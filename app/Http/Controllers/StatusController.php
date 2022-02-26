5ce9df2351026d9345d7004be231e1674b6ab236
tadashimasuda <tmasu4260@gmail.com>
2022-02-26 21:45:18 +0900

    statusがない場合の対応

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -11,11 +11,27 @@ class StatusController extends Controller
 {
     public function update($request,$earnings_total)
     {
-        //データがある時
         $today = Carbon::today();
         Status::where('user_id', $request->user()->id)->whereDate('created_at', $today)->update([
             'days_earnings_total' => DB::raw("days_earnings_total + '$earnings_total'"),
             'days_earnings_qty' => DB::raw("days_earnings_qty + 1"),
+        ]);
+    }
+
+    public function is_status($user_id)
+    {
+        $today = Carbon::today();
+
+        return DB::table('statuses')->where('user_id', $user_id)->whereDate('created_at', $today)->exists();
+    }
+
+    public function store($request,$earnings_total,$prefecture_id)
+    {
+        Status::create([
+            'user_id' => $request->user()->id,
+            'days_earnings_total' => $earnings_total,
+            'days_earnings_qty' => 1,
+            'prefecture_id' => $prefecture_id
         ]);
     }
 }
