b3e4dc89cf385391868a869d3692b9622b339909
tadashimasuda <tmasu4260@gmail.com>
2022-03-10 23:30:04 +0900

    orderResourceの修正

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -16,13 +16,11 @@ class OrderResource extends JsonResource
     {
         return[
             'id' => $this->id,
-            'user_id' => $this->user_id,
             'prefecture_id' => $this->prefecture_id,
             'earnings_incentive' => $this->earnings_incentive,
             'earnings_base' => $this->earnings_base,
             'earnings_total' => $this->earnings_total,
-            'created_at' => $this->created_at->format('Y/m/d H:i:s'),
-            'updated_at' => $this->updated_at->format('Y/m/d H:i:s'),
+            'created_at' => $this->created_at->format('H時i分'),
         ];
     }
 }
