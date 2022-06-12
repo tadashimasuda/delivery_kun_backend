328b7e88c9d889d144acc3cd48a114213e119223
tadashimasuda <tmasu4260@gmail.com>
2022-06-12 23:02:09 +0900

    add:お知らせ一覧の取得

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AnnouncementRequest;
+use App\Http\Resources\AnnouncementResource;
 use App\Models\Announcement;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -25,5 +26,22 @@ class AnnouncementController extends Controller
         ]);
 
         return response()->json(null,201);
+    }
+
+    public function index(Request $request)
+    {
+        $announcements =  Announcement::with('reads')->get();
+
+        foreach($announcements as $announcement){
+            foreach($announcement->reads as $read){
+                if($request->user()->id == $read->user_id && $read->read == 1){
+                    $announcement['read'] = 1;
+                }else{
+                    $announcement['read'] = 0;
+                }
+            }
+        }
+
+        return AnnouncementResource::collection($announcements);
     }
 }
---
+++ app/Http/Resources/AnnouncementResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AnnouncementResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'createdAt' => $this->created_at->format('m月d日'),
+            'read' => $this->read
+        ];
+    }
+}
